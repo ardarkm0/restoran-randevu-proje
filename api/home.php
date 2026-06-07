e382fefcfc1d148997d9211023bdcd31dd44d142
ardarkm0 <ardaarkm@hotmail.com>
2026-06-07 13:10:46 +0300

    Refactor: change 'Sign In' to 'Login'

--- api/home.php
+++ api/home.php
@@ -59,7 +59,7 @@ $userName = $isLoggedIn ? $_COOKIE['user_name'] : '';
                 <?php else: ?>
                     <button class="login-btn" id="loginBtn" onclick="window.location.href='index.html'">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                        Sign In
+                        Login
                     </button>
                 <?php endif; ?>
             </div>
